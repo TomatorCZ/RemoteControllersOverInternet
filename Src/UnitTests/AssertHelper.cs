e2cccfcb6d0033560af896e8c268801bd9b18f9d
Tomas Husak <tomikhusak@seznam.com>
2020-08-20 15:18:19 +0200

    Unittests

---
+++ Src/UnitTests/AssertHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    public static class AssertHelper
+    {
+        public static void AreEqual(byte[] expected, byte[] actual)
+        {
+            if (expected == null || actual == null)
+            {
+                if (expected == null && actual == null)
+                    return;
+                else
+                    throw new AssertFailedException("Null reference");
+            }
+
+            if (expected.Length != actual.Length)
+                throw new AssertFailedException("Lengths are different");
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                if (expected[i] != actual[i])
+                    throw new AssertFailedException("Items are different");
+            }
+        }
+    }
+}
---
+++ Src/UnitTests/ByteArrayExtensionsTest.cs
@@ -0,0 +1,104 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoteControllers;
+using System.Diagnostics;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ByteArrayExtensionsTest
+    {
+        [TestMethod]
+        public void Split_1()
+        {
+            //Arrange
+            byte[] sample = new byte[0];
+
+            //Act
+            byte[][] result = sample.Split(0, 1);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Length);
+        }
+
+        [TestMethod]
+        public void Split_2()
+        {
+            //Arrange
+            byte[] sample = new byte[8] { 1,0,1,0,1,1,1,0};
+            byte[][] expected = new byte[][] 
+            { 
+                new byte[1] { 1},
+                new byte[1] { 1},
+                new byte[3] { 1,1,1} 
+            };
+
+            //Act
+            byte[][] result = sample.Split(0, 3);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected.Length, result.Length);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                AssertHelper.AreEqual(expected[i], result[i]);
+            } 
+        }
+
+        [TestMethod]
+        public void SubArray_1()
+        {
+            //Arrange
+            byte[] sample = new byte[10] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 1 };
+            byte[] expected = new byte[10] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 1 };
+            
+            //Act
+            byte[] result = sample.SubArray(0, 10);
+
+            //Assert
+            AssertHelper.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void SubArray_2()
+        {
+            //Arrange
+            byte[] sample = new byte[10] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 1 };
+            byte[] expected = new byte[0];
+
+            //Act
+            byte[] result = sample.SubArray(0, 0);
+
+            //Assert
+            AssertHelper.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void SubArray_3()
+        {
+            //Arrange
+            byte[] sample = new byte[10] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 1 };
+            byte[] expected = new byte[6] { 1, 2, 1, 2, 1, 1 };
+
+            //Act
+            byte[] result = sample.SubArray(4, 6);
+
+            //Assert
+            AssertHelper.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void SubArray_4()
+        {
+            //Arrange
+            byte[] sample = new byte[10] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 1 };
+            byte[] expected = new byte[0];
+
+            //Act
+            byte[] result = sample.SubArray(9, 0);
+
+            //Assert
+            AssertHelper.AreEqual(expected, result);
+        }
+    }
+}
